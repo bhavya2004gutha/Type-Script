66f27789e16e47aa81ac633e83a8511f2888f9c8
bhavya2004gutha <bhavya.gutha@tekreant.com>
2025-09-10 19:33:06 +0530

    Add files via upload

--- types.ts
+++ types.ts
@@ -1,21 +1,27 @@
 function add (a: number,b:number): number{
     return a+b;
-
 }
 console.log(add(2,6));
 function sub (a: number,b:number): number{
     return a-b;
-
 }
 console.log(sub(9,6));
-
 let numbers: number[] = [1,2,3,4];
 numbers.push(5);
 console.log(numbers);
+let fruits: string[] = ["apple", "banana", "kiwi"];
 
 
-let fruits: string[] = ["apple,Banana,Kiwi"]
 fruits.push("dragon fruit");
 console.log(fruits);
+
+let mixed: (string | number)[] = ["apple", 1, "banana", 2];
+mixed.push("kiwi");
+mixed.push(3);
+console.log(mixed);
+
+
+
+
 
 
